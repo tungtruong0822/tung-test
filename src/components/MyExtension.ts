332cd963cd4cf5f522ec5c5e72cb9902b8e1393b
tungtruong0822 <tungtruong0822@gmai.com>
2023-10-09 14:13:32 +0700

    testt

--- src/components/MyExtension.ts
+++ src/components/MyExtension.ts
@@ -1,51 +1,121 @@
+import Autodesk from 'forge-viewer';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import PropertyPanel from './PropertyPanel'; // Import the PropertyPanel component
+
 export class MyExtension extends Autodesk.Viewing.Extension {
-    constructor(viewer: Autodesk.Viewing.GuiViewer3D, options?: any) {
-        super(viewer, options);
-        this.viewer = viewer;
-        this.options = options;
-        this.subToolbar = null;
+  constructor(viewer, options) {
+    super(viewer, options);
+    this.viewer = viewer;
+    this.options = options;
+    this.subToolbar = null;
+    this.propertyPanel = null; // Reference to the property panel
+    this.isOpen = false; // Property panel initial state
+    this.selectedIds = []; // Store selected object IDs
+  }
+
+  load() {
+    console.log('My excel extension loaded');
+    return true;
+  }
+
+  unload() {
+    if (this.subToolbar) {
+      this.viewer.toolbar.removeControl(this.subToolbar);
+      this.subToolbar = null;
+    }
+    this.closePropertyPanel(); // Close the property panel when unloading the extension
+    return true;
+  }
+
+  onToolbarCreated(toolbar) {
+    // Button 1
+    const button1 = new Autodesk.Viewing.UI.Button('excel-button');
+    button1.icon.style.backgroundImage =
+      'url(https://img.icons8.com/color/30/000000/microsoft-excel-2019--v1.png)';
+    button1.setToolTip('Export Excel');
+
+    // SubToolbar
+    this.subToolbar = new Autodesk.Viewing.UI.ControlGroup('excel-button');
+    this.subToolbar.addControl(button1);
+
+    this.viewer.toolbar.addControl(this.subToolbar);
+
+    // Handle button click event
+    button1.onClick = () => {
+      // Toggle the property panel when the button is clicked
+      this.togglePropertyPanel();
+    };
+  }
+
+  togglePropertyPanel() {
+    // Toggle the property panel's visibility
+    this.isOpen = !this.isOpen;
+
+    if (this.isOpen) {
+      this.openPropertyPanel();
+    } else {
+      this.closePropertyPanel();
+    }
+  }
+
+  openPropertyPanel() {
+    // Create the property panel if it doesn't exist
+    if (!this.propertyPanel) {
+      this.propertyPanel = document.createElement('div');
+      this.propertyPanel.id = 'property-panel';
+      this.propertyPanel.style.position = 'absolute';
+      this.propertyPanel.style.top = '50px';
+      this.propertyPanel.style.right = '20px';
+      this.propertyPanel.style.width = '300px';
+      this.propertyPanel.style.height = '400px';
+      this.propertyPanel.style.backgroundColor = 'white';
+      this.propertyPanel.style.border = '1px solid #ccc';
+      this.propertyPanel.style.zIndex = '1000';
+      this.propertyPanel = new Tabulator('.datagrid-container', {
+        height: '100%',
+        layout: 'fitColumns',
+        groupBy: 'group',
+        columns: [
+            { title: 'Name', field: 'name', width: 150 },
+            { title: 'Volume', field: 'volume', hozAlign: 'left', formatter: 'progress' },
+
+        ]
+    });
+
+
+
+      // Append the property panel to the viewer's container
+      this.viewer.container.appendChild(this.propertyPanel);
     }
 
+    // Render the React PropertyPanel component inside the created div
+    ReactDOM.render(
+      <PropertyPanel selectedIds={this.selectedIds} viewer={this.viewer} />,
+      this.propertyPanel
+    );
+  }
 
-    load() {
-        console.log('My excel extension loaded');
-        return true;
+  closePropertyPanel() {
+    // Remove the property panel and its React component
+    if (this.propertyPanel) {
+      ReactDOM.unmountComponentAtNode(this.propertyPanel);
+      this.propertyPanel.remove();
+      this.propertyPanel = null;
     }
+  }
 
-    unload() {
-        if (this.subToolbar) {
-            this.viewer.toolbar.removeControl(this.subToolbar);
-            this.subToolbar = null;
-        }
-
+  // Handle selection change in the viewer
+  onSelectionChanged(event) {
+    this.selectedIds = event.dbIdArray || []; // Update selected object IDs
+    if (this.isOpen) {
+      this.openPropertyPanel(); // Update the property panel when selection changes
     }
+  }
 
-    onToolbarCreated(toolbar) {
-        // Button 1
-        var button1 = new Autodesk.Viewing.UI.Button('excel-button');
-        button1.icon.style.backgroundImage = 'url(https://img.icons8.com/color/30/000000/microsoft-excel-2019--v1.png)';
-
-        button1.setToolTip('Export Excel');
-
-        // SubToolbar
-        this.subToolbar = new Autodesk.Viewing.UI.ControlGroup('excel-button');
-        this.subToolbar.addControl(button1);
-
-        this.viewer.toolbar.addControl(this.subToolbar);
-
-        // Handle button click event
-        button1.onClick = (event) => {
-            // Your code to handle the button click event
-            // You can show a popup, perform an action, etc.
-
-            console.log('Excel button clicked');
-        };
-    }
-
-    static getName() {
-        return 'MyExtension';
-    }
-
+  static getName() {
+    return 'MyExtension';
+  }
 }
 
 Autodesk.Viewing.theExtensionManager.registerExtension('MyExtension', MyExtension);
--- src/components/PropertyPanel.tsx
+++ src/components/PropertyPanel.tsx
@@ -2,39 +2,65 @@ import React, { useEffect, useState } from 'react';
 
 const PropertyPanel = ({ selectedIds, viewer }) => {
   const [properties, setProperties] = useState([]);
+  const [totalVolume, setTotalVolume] = useState(0);
 
   useEffect(() => {
     if (selectedIds.length === 0) {
       setProperties([]);
+      setTotalVolume(0);
       return;
     }
 
-    const propertyDb = viewer.model.getPropertyDb();
-    const objectProperties = selectedIds.map((objectId) => {
-      const props = propertyDb.getProperties(objectId);
-      return { objectId, properties: props };
-    });
+    const calculateTotalVolume = async () => {
+      try {
+        const data = await new Promise((resolve, reject) => {
+          viewer.model.getExternalIdMapping(resolve, reject);
+        });
 
-    setProperties(objectProperties);
+        const arrayVolume = [];
+        let total = 0;
+
+        for (const key in data) {
+          const dbId = data[key];
+          const properties = await new Promise((resolve, reject) => {
+            viewer.getProperties(dbId, resolve, reject);
+          });
+
+          if (properties) {
+            const propertiesObj = properties.properties;
+            propertiesObj.forEach((obj) => {
+              if (obj.displayName === 'Volume') {
+                const volume = parseFloat(obj.displayValue);
+                if (!isNaN(volume)) {
+                  arrayVolume.push(volume);
+                  total += volume;
+                }
+              }
+            });
+          }
+        }
+
+        setTotalVolume(total);
+        setProperties(arrayVolume);
+      } catch (error) {
+        console.error('Error calculating total volume:', error);
+      }
+    };
+
+    calculateTotalVolume();
   }, [selectedIds, viewer]);
 
   return (
     <div className="property-panel">
       <h3>Selected Object Properties:</h3>
       <ul>
-        {properties.map(({ objectId, properties }) => (
-          <li key={objectId}>
-            <h4>Object ID: {objectId}</h4>
-            <ul>
-              {properties.map((property, index) => (
-                <li key={index}>
-                  {property.displayName}: {property.displayValue}
-                </li>
-              ))}
-            </ul>
+        {properties.map((volume, index) => (
+          <li key={index}>
+            <h4>Object {index + 1} Volume: {volume}</h4>
           </li>
         ))}
       </ul>
+      <h3>Total Volume: {totalVolume}</h3>
     </div>
   );
 };
